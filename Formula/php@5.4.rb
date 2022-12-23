0fc3ceb443193d88ada5972063b43f0d309793dd
luzhuang <401301965@qq.com>
2022-12-23 23:30:30 +0800

    init

--- Formula/php@5.4.rb
+++ Formula/php@5.4.rb
@@ -70,260 +70,260 @@ class PhpAT54 < Formula
       # apxs will interpolate the @ in the versioned prefix: https://bz.apache.org/bugzilla/show_bug.cgi?id=61944
       s.gsub! "LIBEXECDIR='$APXS_LIBEXECDIR'",
               "LIBEXECDIR='" + "#{lib}/httpd/modules".gsub("@", "\\@") + "'"
+    end
+
+    # Update error message in apache sapi to better explain the requirements
+    # of using Apache http in combination with php if the non-compatible MPM
+    # has been selected. Homebrew has chosen not to support being able to
+    # compile a thread safe version of PHP and therefore it is not
+    # possible to recompile as suggested in the original message
+    inreplace "sapi/apache2handler/sapi_apache2.c",
+            "You need to recompile PHP.",
+            "Homebrew PHP does not support a thread-safe php binary. " \
+            "To use the PHP apache sapi please change " \
+            "your httpd config to use the prefork MPM"
+
+    inreplace "sapi/fpm/php-fpm.conf.in", ";daemonize = yes", "daemonize = no"
+
+    # API compatibility with tidy-html5 v5.0.0 - https://github.com/htacg/tidy-html5/issues/224
+    inreplace "ext/tidy/tidy.c", "buffio.h", "tidybuffio.h"
+
+    # Required due to icu4c dependency
+    ENV.cxx11
+
+    # icu4c 61.1 compatability
+    ENV.append "CPPFLAGS", "-DU_USING_ICU_NAMESPACE=1"
+
+    config_path = etc/"php/#{php_version}"
+    # Prevent system pear config from inhibiting pear install
+    (config_path/"pear.conf").delete if (config_path/"pear.conf").exist?
+
+    # Prevent homebrew from harcoding path to sed shim in phpize script
+    ENV["lt_cv_path_SED"] = "sed"
+
+    # Each extension that is built on Mojave needs a direct reference to the
+    # sdk path or it won't find the headers
+    headers_path = ""
+    headers_path = "=#{MacOS.sdk_path_if_needed}/usr" if OS.mac?
+
+    args = %W[
+    --prefix=#{prefix}
+    --localstatedir=#{var}
+    --sysconfdir=#{config_path}
+    --with-config-file-path=#{config_path}
+    --with-config-file-scan-dir=#{config_path}/conf.d
+    --with-pear=#{pkgshare}/pear
+    --enable-bcmath
+    --enable-calendar
+    --enable-dba
+    --enable-exif
+    --enable-ftp
+    --enable-fpm
+    --enable-intl
+    --enable-mbregex
+    --enable-mbstring
+    --enable-mysqlnd
+    --enable-opcache
+    --enable-pcntl
+    --enable-phpdbg
+    --enable-shmop
+    --enable-soap
+    --enable-sockets
+    --enable-sysvmsg
+    --enable-sysvsem
+    --enable-sysvshm
+    --enable-wddx
+    --enable-zip
+    --with-apxs2=#{Formula["httpd"].opt_bin}/apxs
+    --with-curl=#{Formula["curl"].opt_prefix}
+    --with-fpm-user=_www
+    --with-fpm-group=_www
+    --with-freetype-dir=#{Formula["freetype"].opt_prefix}
+    --with-gd
+    --with-gettext=#{Formula["gettext"].opt_prefix}
+    --with-gmp=#{Formula["gmp"].opt_prefix}
+    --with-iconv#{headers_path}
+    --with-icu-dir=#{Formula["icu4c"].opt_prefix}
+    --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
+    --with-kerberos#{headers_path}
+    --with-layout=GNU
+    --with-ldap=#{Formula["openldap"].opt_prefix}
+    --with-ldap-sasl#{headers_path}
+    --with-libzip
+    --with-mhash#{headers_path}
+    --with-mysql-sock=/tmp/mysql.sock
+    --with-mysqli=mysqlnd
+    --with-mysql=mysqlnd
+    --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
+    --with-pdo-dblib=#{Formula["freetds"].opt_prefix}
+    --with-pdo-mysql=mysqlnd
+    --with-pdo-odbc=unixODBC,#{Formula["unixodbc"].opt_prefix}
+    --with-pdo-pgsql=#{Formula["libpq"].opt_prefix}
+    --with-pdo-sqlite=#{Formula["sqlite"].opt_prefix}
+    --with-pgsql=#{Formula["libpq"].opt_prefix}
+    --with-pic
+    --with-png-dir=#{Formula["libpng"].opt_prefix}
+    --with-pspell=#{Formula["aspell"].opt_prefix}
+    --with-sqlite3=#{Formula["sqlite"].opt_prefix}
+    --with-tidy=#{Formula["tidy-html5"].opt_prefix}
+    --with-unixODBC=#{Formula["unixodbc"].opt_prefix}
+    --with-xmlrpc
+    ]
+
+    if OS.mac?
+    args << "--with-bz2#{headers_path}"
+    args << "--with-libedit#{headers_path}"
+    args << "--with-libxml-dir#{headers_path}"
+    args << "--with-ndbm#{headers_path}"
+    args << "--with-xsl#{headers_path}"
+    args << "--with-zlib#{headers_path}"
+    end
+
+    if OS.linux?
+    args << "--with-zlib=#{Formula["zlib"].opt_prefix}"
+    args << "--with-bzip2=#{Formula["bzip2"].opt_prefix}"
+    args << "--with-libedit=#{Formula["libedit"].opt_prefix}"
+    args << "--with-libxml-dir=#{Formula["libxml2"].opt_prefix}"
+    args << "--with-xsl=#{Formula["libxslt"].opt_prefix}"
+    args << "--without-ldap-sasl"
+    args << "--without-ndbm"
+    args << "--without-gdbm"
+    end
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    # Allow pecl to install outside of Cellar
+    extension_dir = Utils.safe_popen_read("#{bin}/php-config", "--extension-dir").chomp
+    orig_ext_dir = File.basename(extension_dir)
+    inreplace bin/"php-config", lib/"php", prefix/"pecl"
+    %w[development production].each do |mode|
+    inreplace "php.ini-#{mode}", %r{; ?extension_dir = "\./"},
+      "extension_dir = \"#{HOMEBREW_PREFIX}/lib/php/pecl/#{orig_ext_dir}\""
+    end
+
+    # Use OpenSSL cert bundle
+    openssl = Formula["openssl@1.1"]
+    %w[development production].each do |mode|
+    inreplace "php.ini-#{mode}", /; ?openssl\.cafile=/,
+      "openssl.cafile = \"#{openssl.pkgetc}/cert.pem\""
+    inreplace "php.ini-#{mode}", /; ?openssl\.capath=/,
+      "openssl.capath = \"#{openssl.pkgetc}/certs\""
+    end
+
+    config_files = {
+    "php.ini-development"   => "php.ini",
+    "php.ini-production"    => "php.ini-production",
+    "sapi/fpm/php-fpm.conf" => "php-fpm.conf",
+    }
+    config_files.each_value do |dst|
+    dst_default = config_path/"#{dst}.default"
+    rm dst_default if dst_default.exist?
+    end
+    config_path.install config_files
+
+    unless (var/"log/php-fpm.log").exist?
+    (var/"log").mkpath
+    touch var/"log/php-fpm.log"
+    end
   end
 
-  # Update error message in apache sapi to better explain the requirements
-      # of using Apache http in combination with php if the non-compatible MPM
-      # has been selected. Homebrew has chosen not to support being able to
-      # compile a thread safe version of PHP and therefore it is not
-      # possible to recompile as suggested in the original message
-      inreplace "sapi/apache2handler/sapi_apache2.c",
-                "You need to recompile PHP.",
-                "Homebrew PHP does not support a thread-safe php binary. " \
-                "To use the PHP apache sapi please change " \
-                "your httpd config to use the prefork MPM"
+  def post_install
+    pear_prefix = pkgshare/"pear"
+    pear_files = %W[
+      #{pear_prefix}/.depdblock
+      #{pear_prefix}/.filemap
+      #{pear_prefix}/.depdb
+      #{pear_prefix}/.lock
+    ]
 
-      inreplace "sapi/fpm/php-fpm.conf.in", ";daemonize = yes", "daemonize = no"
-
-      # API compatibility with tidy-html5 v5.0.0 - https://github.com/htacg/tidy-html5/issues/224
-      inreplace "ext/tidy/tidy.c", "buffio.h", "tidybuffio.h"
-
-      # Required due to icu4c dependency
-      ENV.cxx11
-
-      # icu4c 61.1 compatability
-      ENV.append "CPPFLAGS", "-DU_USING_ICU_NAMESPACE=1"
-
-      config_path = etc/"php/#{php_version}"
-      # Prevent system pear config from inhibiting pear install
-      (config_path/"pear.conf").delete if (config_path/"pear.conf").exist?
-
-      # Prevent homebrew from harcoding path to sed shim in phpize script
-      ENV["lt_cv_path_SED"] = "sed"
-
-      # Each extension that is built on Mojave needs a direct reference to the
-      # sdk path or it won't find the headers
-      headers_path = ""
-      headers_path = "=#{MacOS.sdk_path_if_needed}/usr" if OS.mac?
-
-      args = %W[
-        --prefix=#{prefix}
-        --localstatedir=#{var}
-        --sysconfdir=#{config_path}
-        --with-config-file-path=#{config_path}
-        --with-config-file-scan-dir=#{config_path}/conf.d
-        --with-pear=#{pkgshare}/pear
-        --enable-bcmath
-        --enable-calendar
-        --enable-dba
-        --enable-exif
-        --enable-ftp
-        --enable-fpm
-        --enable-intl
-        --enable-mbregex
-        --enable-mbstring
-        --enable-mysqlnd
-        --enable-opcache
-        --enable-pcntl
-        --enable-phpdbg
-        --enable-shmop
-        --enable-soap
-        --enable-sockets
-        --enable-sysvmsg
-        --enable-sysvsem
-        --enable-sysvshm
-        --enable-wddx
-        --enable-zip
-        --with-apxs2=#{Formula["httpd"].opt_bin}/apxs
-        --with-curl=#{Formula["curl"].opt_prefix}
-        --with-fpm-user=_www
-        --with-fpm-group=_www
-        --with-freetype-dir=#{Formula["freetype"].opt_prefix}
-        --with-gd
-        --with-gettext=#{Formula["gettext"].opt_prefix}
-        --with-gmp=#{Formula["gmp"].opt_prefix}
-        --with-iconv#{headers_path}
-        --with-icu-dir=#{Formula["icu4c"].opt_prefix}
-        --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
-        --with-kerberos#{headers_path}
-        --with-layout=GNU
-        --with-ldap=#{Formula["openldap"].opt_prefix}
-        --with-ldap-sasl#{headers_path}
-        --with-libzip
-        --with-mhash#{headers_path}
-        --with-mysql-sock=/tmp/mysql.sock
-        --with-mysqli=mysqlnd
-        --with-mysql=mysqlnd
-        --with-openssl=#{Formula["openssl@1.1"].opt_prefix}
-        --with-pdo-dblib=#{Formula["freetds"].opt_prefix}
-        --with-pdo-mysql=mysqlnd
-        --with-pdo-odbc=unixODBC,#{Formula["unixodbc"].opt_prefix}
-        --with-pdo-pgsql=#{Formula["libpq"].opt_prefix}
-        --with-pdo-sqlite=#{Formula["sqlite"].opt_prefix}
-        --with-pgsql=#{Formula["libpq"].opt_prefix}
-        --with-pic
-        --with-png-dir=#{Formula["libpng"].opt_prefix}
-        --with-pspell=#{Formula["aspell"].opt_prefix}
-        --with-sqlite3=#{Formula["sqlite"].opt_prefix}
-        --with-tidy=#{Formula["tidy-html5"].opt_prefix}
-        --with-unixODBC=#{Formula["unixodbc"].opt_prefix}
-        --with-xmlrpc
-      ]
-
-      if OS.mac?
-        args << "--with-bz2#{headers_path}"
-        args << "--with-libedit#{headers_path}"
-        args << "--with-libxml-dir#{headers_path}"
-        args << "--with-ndbm#{headers_path}"
-        args << "--with-xsl#{headers_path}"
-        args << "--with-zlib#{headers_path}"
-      end
-
-      if OS.linux?
-        args << "--with-zlib=#{Formula["zlib"].opt_prefix}"
-        args << "--with-bzip2=#{Formula["bzip2"].opt_prefix}"
-        args << "--with-libedit=#{Formula["libedit"].opt_prefix}"
-        args << "--with-libxml-dir=#{Formula["libxml2"].opt_prefix}"
-        args << "--with-xsl=#{Formula["libxslt"].opt_prefix}"
-        args << "--without-ldap-sasl"
-        args << "--without-ndbm"
-        args << "--without-gdbm"
-      end
-
-      system "./configure", *args
-      system "make"
-      system "make", "install"
-
-      # Allow pecl to install outside of Cellar
-      extension_dir = Utils.safe_popen_read("#{bin}/php-config", "--extension-dir").chomp
-      orig_ext_dir = File.basename(extension_dir)
-      inreplace bin/"php-config", lib/"php", prefix/"pecl"
-      %w[development production].each do |mode|
-        inreplace "php.ini-#{mode}", %r{; ?extension_dir = "\./"},
-          "extension_dir = \"#{HOMEBREW_PREFIX}/lib/php/pecl/#{orig_ext_dir}\""
-      end
-
-      # Use OpenSSL cert bundle
-      openssl = Formula["openssl@1.1"]
-      %w[development production].each do |mode|
-        inreplace "php.ini-#{mode}", /; ?openssl\.cafile=/,
-          "openssl.cafile = \"#{openssl.pkgetc}/cert.pem\""
-        inreplace "php.ini-#{mode}", /; ?openssl\.capath=/,
-          "openssl.capath = \"#{openssl.pkgetc}/certs\""
-      end
-
-      config_files = {
-        "php.ini-development"   => "php.ini",
-        "php.ini-production"    => "php.ini-production",
-        "sapi/fpm/php-fpm.conf" => "php-fpm.conf",
-      }
-      config_files.each_value do |dst|
-        dst_default = config_path/"#{dst}.default"
-        rm dst_default if dst_default.exist?
-      end
-      config_path.install config_files
-
-      unless (var/"log/php-fpm.log").exist?
-        (var/"log").mkpath
-        touch var/"log/php-fpm.log"
-      end
+    %W[
+      #{pear_prefix}/.channels
+      #{pear_prefix}/.channels/.alias
+    ].each do |f|
+      chmod 0755, f
+      pear_files.concat(Dir["#{f}/*"])
     end
 
-    def post_install
-      pear_prefix = pkgshare/"pear"
-      pear_files = %W[
-        #{pear_prefix}/.depdblock
-        #{pear_prefix}/.filemap
-        #{pear_prefix}/.depdb
-        #{pear_prefix}/.lock
-      ]
+    chmod 0644, pear_files
 
-      %W[
-        #{pear_prefix}/.channels
-        #{pear_prefix}/.channels/.alias
-      ].each do |f|
-        chmod 0755, f
-        pear_files.concat(Dir["#{f}/*"])
-      end
+    # Custom location for extensions installed via pecl
+    pecl_path = HOMEBREW_PREFIX/"lib/php/pecl"
+    pecl_path.mkpath
+    ln_s pecl_path, prefix/"pecl" unless (prefix/"pecl").exist?
+    extension_dir = Utils.safe_popen_read("#{bin}/php-config", "--extension-dir").chomp
+    php_basename = File.basename(extension_dir)
+    php_ext_dir = opt_prefix/"lib/php"/php_basename
 
-      chmod 0644, pear_files
+    # fix pear config to install outside cellar
+    pear_path = HOMEBREW_PREFIX/"share/pear@#{php_version}"
+    cp_r pkgshare/"pear/.", pear_path
+    {
+      "php_ini"  => etc/"php/#{php_version}/php.ini",
+      "php_dir"  => pear_path,
+      "doc_dir"  => pear_path/"doc",
+      "ext_dir"  => pecl_path/php_basename,
+      "bin_dir"  => opt_bin,
+      "data_dir" => pear_path/"data",
+      "cfg_dir"  => pear_path/"cfg",
+      "www_dir"  => pear_path/"htdocs",
+      "man_dir"  => HOMEBREW_PREFIX/"share/man",
+      "test_dir" => pear_path/"test",
+      "php_bin"  => opt_bin/"php",
+    }.each do |key, value|
+      value.mkpath if /(?<!bin|man)_dir$/.match?(key)
+      system bin/"pear", "config-set", key, value, "system"
+    end
 
-      # Custom location for extensions installed via pecl
-      pecl_path = HOMEBREW_PREFIX/"lib/php/pecl"
-      pecl_path.mkpath
-      ln_s pecl_path, prefix/"pecl" unless (prefix/"pecl").exist?
-      extension_dir = Utils.safe_popen_read("#{bin}/php-config", "--extension-dir").chomp
-      php_basename = File.basename(extension_dir)
-      php_ext_dir = opt_prefix/"lib/php"/php_basename
+    system bin/"pear", "update-channels"
 
-      # fix pear config to install outside cellar
-      pear_path = HOMEBREW_PREFIX/"share/pear@#{php_version}"
-      cp_r pkgshare/"pear/.", pear_path
-      {
-        "php_ini"  => etc/"php/#{php_version}/php.ini",
-        "php_dir"  => pear_path,
-        "doc_dir"  => pear_path/"doc",
-        "ext_dir"  => pecl_path/php_basename,
-        "bin_dir"  => opt_bin,
-        "data_dir" => pear_path/"data",
-        "cfg_dir"  => pear_path/"cfg",
-        "www_dir"  => pear_path/"htdocs",
-        "man_dir"  => HOMEBREW_PREFIX/"share/man",
-        "test_dir" => pear_path/"test",
-        "php_bin"  => opt_bin/"php",
-      }.each do |key, value|
-        value.mkpath if /(?<!bin|man)_dir$/.match?(key)
-        system bin/"pear", "config-set", key, value, "system"
-      end
-
-      system bin/"pear", "update-channels"
-
-      %w[
-        opcache
-      ].each do |e|
-        ext_config_path = etc/"php/#{php_version}/conf.d/ext-#{e}.ini"
-        extension_type = (e == "opcache") ? "zend_extension" : "extension"
-        if ext_config_path.exist?
-          inreplace ext_config_path,
-            /#{extension_type}=.*$/, "#{extension_type}=#{php_ext_dir}/#{e}.so"
-        else
-          ext_config_path.write <<~EOS
-            [#{e}]
-            #{extension_type}="#{php_ext_dir}/#{e}.so"
-          EOS
-        end
+    %w[
+      opcache
+    ].each do |e|
+      ext_config_path = etc/"php/#{php_version}/conf.d/ext-#{e}.ini"
+      extension_type = (e == "opcache") ? "zend_extension" : "extension"
+      if ext_config_path.exist?
+        inreplace ext_config_path,
+          /#{extension_type}=.*$/, "#{extension_type}=#{php_ext_dir}/#{e}.so"
+      else
+        ext_config_path.write <<~EOS
+          [#{e}]
+          #{extension_type}="#{php_ext_dir}/#{e}.so"
+        EOS
       end
     end
+  end
 
-    def caveats
-      <<~EOS
-        To enable PHP in Apache add the following to httpd.conf and restart Apache:
-            LoadModule php5_module #{opt_lib}/httpd/modules/libphp5.so
+  def caveats
+    <<~EOS
+      To enable PHP in Apache add the following to httpd.conf and restart Apache:
+          LoadModule php5_module #{opt_lib}/httpd/modules/libphp5.so
 
-            <FilesMatch \\.php$>
-                SetHandler application/x-httpd-php
-            </FilesMatch>
+          <FilesMatch \\.php$>
+              SetHandler application/x-httpd-php
+          </FilesMatch>
 
-        Finally, check DirectoryIndex includes index.php
-            DirectoryIndex index.php index.html
+      Finally, check DirectoryIndex includes index.php
+          DirectoryIndex index.php index.html
 
-        The php.ini and php-fpm.ini file can be found in:
-            #{etc}/php/#{php_version}/
-      EOS
-    end
+      The php.ini and php-fpm.ini file can be found in:
+          #{etc}/php/#{php_version}/
+    EOS
+  end
 
-    def php_version
-      version.to_s.split(".")[0..1].join(".")
-    end
+  def php_version
+    version.to_s.split(".")[0..1].join(".")
+  end
 
-    plist_options manual: "php-fpm"
-    service do
-      run [opt_sbin/"php-fpm", "--nodaemonize"]
-      run_type :immediate
-      keep_alive true
-      error_log_path var/"log/php-fpm.log"
-      working_dir var
-    end
+  plist_options manual: "php-fpm"
+  service do
+    run [opt_sbin/"php-fpm", "--nodaemonize"]
+    run_type :immediate
+    keep_alive true
+    error_log_path var/"log/php-fpm.log"
+    working_dir var
+  end
 
   test do
     assert_match(/^Zend OPcache$/, shell_output("#{bin}/php -i"),
