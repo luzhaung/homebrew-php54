9a4824fa64b730e15e3606f20b5e52d2d9ce0e9e
luzhuang <401301965@qq.com>
2022-12-23 23:49:07 +0800

    init

--- Formula/php@5.4.rb
+++ Formula/php@5.4.rb
@@ -142,7 +142,7 @@ class PhpAT54 < Formula
     --with-gd
     --with-gettext=#{Formula["gettext"].opt_prefix}
     --with-gmp=#{Formula["gmp"].opt_prefix}
-    --with-iconv#{Formula["libiconv"].opt_prefix}
+    --with-iconv=#{Formula["libiconv"].opt_prefix}
     --with-icu-dir=#{Formula["icu4c"].opt_prefix}
     --with-jpeg-dir=#{Formula["jpeg"].opt_prefix}
     --with-kerberos#{headers_path}
